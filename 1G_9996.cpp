e9b5554e91c673835723f464ead5eac87de3c13a
= <=>
2023-04-02 01:53:25 +0900

    1G_9996 3rd try - using find(), substr()

--- 1G_9996.cpp
+++ 1G_9996.cpp
@@ -2,63 +2,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n; // number
-int a; // index of asterisk
-int l; // remained length after asterisk
-string nm; // name
-vector<string> fs; //files
-string s;
+int n;
+int pos;
+string p, pre, suf, s; //pattern
 int main () {
     cin >> n;
-    cin >> nm;
+    cin >> p;
+    pos = p.find('*');
+    string pre = p.substr(0, pos); // from, till
+    string suf = p.substr(pos + 1, p.length()); 
 
     for (int i = 0; i < n; ++i) {
         cin >> s;
-        fs.push_back(s);
-    }
 
-    for (int i = 0; i < nm.length(); ++i) {
-        if (nm[i] == '*') {
-            a = i; // idx of asterisk
-            l = nm.length() - a - 1;
-        }
-    }
-
-    for (int i = 0; i < n; ++i) {
-        int start = 0;
-        int as = a;
-        int al = fs[i].length() - l;
-        int end = fs[i].length();
-
-        bool isMatch = true;
-        
-        if (end < nm.size() - 1) isMatch = false;
-
-        if (isMatch) {
-            for (int j = 0; j < as; ++j) {
-                // cout << j << " " << fs[i][j] << " " << nm[j] << "\n";
-                if (fs[i][j] == nm[j]) continue;
-                isMatch = false;
-                break;
-            }
+        // Without this condition block, runtime error occurred when checking.
+        if (s.length() < pre.length() + suf.length()) {
+            cout << "NE\n";
+            continue;
         }
 
-        if (isMatch) {
-            for (int j = fs[i].length() - 1; j >= al; --j) {
-                // cout << j << " " << fs[i][j] << " " << nm[j - al + as + 1] << "\n";
-                if (fs[i][j] == nm[j - al + as + 1]) continue;
-                isMatch = false;
-                break;
-            }
-        }
-
-        if (isMatch) {
-            cout << "DA";
+        if (pre == s.substr(0, pre.length()) && suf == s.substr(s.length() - suf.length(), s.length())) {
+            cout << "DA\n";
         } else {
-            cout << "NE";
+            cout << "NE\n";
         }
-
-        cout << "\n";
     }
 
     return 0;
