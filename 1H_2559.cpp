d4031075b2750e64b99bcff6b1b01d8818ccf8c7
= <=>
2023-04-02 21:16:53 +0900

    1H_2559 Second Try - Wrong

--- 1H_2559.cpp
+++ 1H_2559.cpp
@@ -2,23 +2,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dn, sn, m = INT_MIN, d[100000]; // day num, sum num
+int dn, sn, m = INT_MIN, a[100100]; // day num, sum num
 int main () {
     cin >> dn >> sn;
 
     for (int i = 0; i < dn; ++i) {
-        cin >> d[i];
+        cin >> a[i];
     }
 
-    for (int i = sn - 1; i < dn; ++i) {
-        int sum = 0;
-        for (int j = 0; j < sn; ++j) {
-            sum += d[i - j];
-        }
+
+    int sum = 0;
+    for (int i = 0; i < sn; ++i) {
+        sum += a[i];
+    }
+
+    for (int i = sn; i < dn; ++i) {
+        sum -= a[i-sn];
+        sum += a[i];
+
+        // cout << i << "(" << ps<< "," << pe << ") ";
+        // cout << "-(" << a[ps] << ")-("<< a[pe] <<")="<< sum << " ";
         if (m < sum) m = sum;
     }
 
-    cout << m << "\n";
+    cout << m;
 
     return 0;
 }
