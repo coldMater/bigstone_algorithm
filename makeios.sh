a41a6a6e73ce6b49f1cea944ada85cd9c33c7be1
= <=>
2023-04-07 00:46:46 +0900

    edit makeios.sh to make cpp file with problem url

--- makeios.sh
+++ makeios.sh
@@ -1,7 +1,11 @@
-filename="$1" # problem number e.g., "1K_2312"
-iocount="$2" # count of input, output set 
+#!/bin/bash
 
-touch ""$filename".cpp"
+problemNum="$1" # problem number e.g., "1K"
+bjNum="$2"
+filename="${problemNum}_${bjNum}"
+iocount="$3" # count of input, output set 
+
+echo "// https://www.acmicpc.net/problem/1940" >> ""$filename".cpp"
 
 for i in $(seq 1 $iocount)
 do
