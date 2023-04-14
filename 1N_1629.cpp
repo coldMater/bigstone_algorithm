287a58eae6573600b8dbba815e6f608d144129ba
= <=>
2023-04-15 00:20:57 +0900

    1N_1629 - Second Try: Using Recursive & Modular Operation

--- 1N_1629.cpp
+++ 1N_1629.cpp
@@ -1,17 +1,24 @@
-// https://www.acmicpc.net/problem/1940
+// https://www.acmicpc.net/problem/1629
 #include <bits/stdc++.h>
 using namespace std;
 
-int l, r, d, rs; // (left * right) % divider, result
+long long l, r, d; // (left ^ right) % divider, result
+
+long long rec (long long n) {
+    if (n == 1) return l % d;
+    long long ret = rec(n / 2);
+    ret = (ret * ret) % d;
+    if (n % 2) ret = (ret * l) % d;
+    return ret;
+}
+
 int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> l >> r >> d;
 
-    rs = 1;
-    for (int i = 0; i < r; ++i) {
-        rs = (rs * (l % d)) % d;
-    }
-
-    cout << rs;
+    cout << rec(r);
 
     return 0;
 }
