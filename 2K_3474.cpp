d12cf027e51ca41bc7240030c6d10da8c6c7f847
= <=>
2023-05-14 01:26:09 +0900

    2K_3474 - 2nd try: 🚩 Reduce Time Complexity

--- 2K_3474.cpp
+++ 2K_3474.cpp
@@ -8,12 +8,11 @@ int main () {
     for (int i = 0; i < n; ++i) {
         cin >> t;
         int c = 0; // count
-        for (int j = 5; j <= t; j = j + 5) {
-            int t = j; // temp
-            while (t % 5 == 0) {
-                c++;
-                t /= 5;
-            }
+        for (int j = 5; j <= t; j *= 5) {
+            c += t / j; 
+            // when j = 5, count multiples of 5.
+            // when j = 25, count multiples of 25. It is second iteration of this for loop. 25 is 5 times 5, so another five has to be included as count.
+            // and by repeating the power of 5, count number of 5 like this.
         }
         cout << c << "\n";
     }
