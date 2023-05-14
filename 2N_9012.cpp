5308fd2889d3df93a6db8ed3a81c6ba0310a8fd1
= <=>
2023-05-14 14:35:40 +0900

    2N_9012 - 2nd Try: segregate function

--- 2N_9012.cpp
+++ 2N_9012.cpp
@@ -4,31 +4,31 @@ using namespace std;
 
 int n;
 string s;
-stack<char> st;
+
+bool c(string s) {
+    stack<char> st;
+    for (char c : s) {
+        if (c == '(') {
+            st.push(c);
+        } else if (!st.empty()) { // when c == '('
+            st.pop();
+        } else if (st.empty()) { // when c == '('
+            return false;
+        }
+    }
+
+    return st.empty();
+}
+
 int main () {
     cin >> n;
 
     for (int i = 0; i < n; ++i) {
         cin >> s;
-        for (char c : s) {
-            if (st.size() != 0 && st.top() != c) {
-                st.pop();
-            } else {
-                if (st.size() == 0 && c == ')') {
-                    st.push(')');
-                    break;
-                }
-                st.push(c);
-            }
-        }
-        if (st.size() == 0) {
-            cout << "YES" << "\n";
+        if (c(s)) {
+            cout << "YES\n";
         } else {
-            cout << "NO" << "\n";
-        }
-
-        while (st.size() != 0) {
-            st.pop();
+            cout << "NO\n";
         }
     }
     return 0;
