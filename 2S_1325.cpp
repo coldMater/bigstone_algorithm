6a30e89aa53e7499c154525c9c30c6a98b780659
= <=>
2023-05-25 23:44:20 +0900

    2S_1325 - 1st try: Fail

---
+++ 2S_1325.cpp
@@ -0,0 +1,50 @@
+// https://www.acmicpc.net/problem/2S
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int a, b;
+vector<int> l[100'000];
+bool visited[100'000];
+vector<int> reliable;
+int m;
+int cnt;
+
+void dfs (int cn) { // computer number
+    visited[cn] = true;
+    cnt++;
+    // for (int b : l[cn]) { // believer
+    for (size_t i = 0; i < l[cn].size(); ++i) {
+        int n = l[cn][i]; // next computer number
+        if (visited[n]) continue;
+        dfs(n);
+    }
+}
+
+int main () {
+    cin >> N >> M;
+    for (int i = 0; i < M; ++i) {
+        cin >> a >> b;
+        l[b].push_back(a);
+    }
+
+    for (int i = 0; i < N; ++i) {
+        dfs(i);
+        if (m < cnt) {
+            m = cnt;
+            reliable = { i };
+        } else if (cnt != 0 && m == cnt) {
+            reliable.push_back(i);
+        }
+
+        //initialize
+        cnt = 0;
+        fill(begin(visited), end(visited), false);
+    }
+
+    for (int c : reliable) {
+        cout << c << " ";
+    }
+
+    return 0;
+}
