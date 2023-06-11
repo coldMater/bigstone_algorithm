e63871e51e31074c9febfe82ef811e9c98b9c080
= <=>
2023-06-12 01:38:48 +0900

    3D_4179 - nth try - success but struggled ☠️

--- 3D_4179.cpp
+++ 3D_4179.cpp
@@ -5,113 +5,106 @@ using namespace std;
 int R, C;
 string s;
 char m[1000][1000];
-pair<int, int> J; // jihoon's position
+int v[1000][1000];
+int jv[1000][1000]; // jihoon's visited
 
 int dy[4] = { -1, 0, 1, 0 };
 int dx[4] = { 0, 1, 0, -1 }; 
 
-bool escaped;
-bool died;
-int c = 0; // count
-
 void print () {
     for (int i = 0; i < R; ++i) {
         for (int j = 0; j < C; ++j) {
-            cout << m[i][j] << " ";
+            cout << v[i][j];
         }
         cout << "\n";
     }
     cout << "\n";
 }
 
-void fire () {
-    c++;
-    vector<pair<int, int>> jhs; // jihoon's 
+void printJ () {
     for (int i = 0; i < R; ++i) {
         for (int j = 0; j < C; ++j) {
-            if (m[i][j] == 'J') {
-                jhs.push_back({ i, j });
-            }
+            cout << jv[i][j];
         }
+        cout << "\n";
     }
+    cout << "\n";
+}
 
-    if (jhs.size() == 0) {
-        died = true;
-        return;
-    }
+queue<pair<int, int>> fs; // fires
+void bfs () {
+    while (fs.size()) {
+        int y = fs.front().first;
+        int x = fs.front().second;
+        fs.pop();
 
-    for (auto j : jhs) {
-        int y = j.first;
-        int x = j.second;
         for (int i = 0; i < 4; ++i) {
             int ny = y + dy[i];
             int nx = x + dx[i];
             if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
-            if (m[ny][nx] == '#' || m[ny][nx] == 'J' || m[ny][nx] == 'F') continue;
-            m[ny][nx] = 'J';
-        }
-    }
-
-    vector<pair<int, int>> fires;
-    for (int i = 0; i < R; ++i) {
-        for (int j = 0; j < C; ++j) {
-            if (m[i][j] == 'F') {
-                fires.push_back({ i, j });
-            }
-        }
-    }
-
-    for (auto f : fires) {
-        int y = f.first;
-        int x = f.second;
-        for (int i = 0; i < 4; ++i) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
-            if (m[ny][nx] == '#' || m[ny][nx] == 'F') continue;
-            if (m[ny][nx] == 'J') {
-                // Fire reached to jihoon's area
-            }
-            m[ny][nx] = 'F';
-        }
-    }
-
-    for (auto j : jhs) {
-        if (j.first == 0 || j.first >= C - 1 || j.second == 0 || j.second >= R - 1) {
-            // cout << "escpaed! " << j.first << " " << j.second << "\n";
-            escaped = true;
-            return;
+            if (v[ny][nx] != 0 || m[ny][nx] == '#') continue;
+            v[ny][nx] = v[y][x] + 1;
+            fs.push({ ny, nx });
         }
     }
 }
 
+queue<pair<int, int>> jp; // jihoon's position
+int f () {
+    while (jp.size()) {
+        int y = jp.front().first;
+        int x = jp.front().second;
+        jp.pop();
+        if (y == 0 || x == 0 || y == R - 1 || x == C - 1) return jv[y][x];
+
+        for (int i = 0; i < 4; ++i) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
+            if (jv[ny][nx] != 0 || m[ny][nx] == '#') continue;
+            if (v[ny][nx] != 0 && v[ny][nx] - jv[y][x] < 2) continue;
+            jv[ny][nx] = jv[y][x] + 1;
+            // if (ny == 0 || nx == 0 || ny == R - 1 || nx == C - 1) return jv[ny][nx];
+            jp.push({ ny, nx });
+        }
+    }
+    return -1;
+}
+
+
 int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cin.tie(NULL);
     cin >> R >> C;
     for (int i = 0; i < R; ++i) {
         cin >> s;
         for (int j = 0; j < C; ++j) {
             m[i][j] = s[j];
+            if (m[i][j] == 'F') {
+                fs.push({ i, j });
+                v[i][j] = 1;
+            }
+            if (m[i][j] == 'J') {
+                // if (i == R - 1 || j == C - 1 || j == 0 || i == 0) { // 💡 Didn't consider zero index
+                //     cout << 1;
+                //     return 0;
+                // }
+                jp.push({ i, j });
+                jv[i][j] = 1;
+            }
         }
     }
 
-    for (int t = 0; t < max(C, R); ++t) {
-        fire();
-        if (escaped) {
-            cout << c;
-            return 0;
-        }
-
-        if (died) {
-            cout << "IMPOSSIBLE";
-            return 0;
-        }
-
-        if (max(C, R) < t) {
-            cout << "IMPOSSIBLE";
-            return 0;
-        }
+    bfs();
+    // print();
+    // cout << "\n";
+    int r = f();
+    // printJ();
+    if (r == -1) {
+        cout << "IMPOSSIBLE";
+    } else {
+        cout << r;
     }
-    cout << "IMPOSSIBLE";
 
     return 0;
 }
