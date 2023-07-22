2f54067ec5728435ef7a6b09437cea40edb3d1f8
= <=>
2023-07-23 05:03:39 +0900

    3I_17071 - 3rd try: 8% failed and add counterexample

--- 3I_17071.cpp
+++ 3I_17071.cpp
@@ -6,59 +6,43 @@ int sip, bp; // subin's initial position, brother's position
 const int M = 500'001; // MAX
 int visited[M];
 int now;
-void bfs () { // brother's present position
+int bfs () { // brother's present position
   queue<int> q;
   q.push(sip);
   visited[sip] = 1;
 
+  int t = 0;
   while (!q.empty()) {
-    int prev = q.front();
-    q.pop();
+    int qSize = q.size();
+    for (int i = 0; i < qSize; ++i) {
+      int prev = q.front();
+      q.pop();
+      if (prev == bp) return t;
 
-    for (int next : { prev - 1, prev + 1, prev * 2 }) {
-      if (next < 0 || next >= M) continue;
-      if (visited[next] != 0) continue;
-      visited[next] = visited[prev] + 1;
-      q.push(next);
+      for (int next : { prev - 1, prev + 1, prev * 2 }) {
+        if (visited[bp] != 0 && (t - visited[bp] - 1) % 2 == 0) {
+          return t;
+        }
+        if (next < 0 || next >= M) continue;
+        if (visited[next] != 0) continue;
+        visited[next] = visited[prev] + 1;
+        if (visited[bp] != 0 && (t - visited[bp] - 1) % 2 == 0) {
+          return t;
+        }
+        q.push(next);
+      }
     }
+    t++;
+    bp = bp + t;
+    if (bp >= M) return -1;
   }
+  return -1;
 }
 
 int main () {
   cin >> sip >> bp;
 
-  if (sip == bp) {
-    cout << 0;
-    return 0;
-  }
-
-  bfs();
-  int t = 0;
-  int min = 987654321;
-  int minT = 0;
-  while(true) {
-    t++;
-    bp = bp + t;
-    if (bp >= M) {
-      break;
-    }
-    if ((visited[bp] - 1 - t) % 2 == 0) {
-      if (min > (visited[bp] - 1)) { 
-        min = visited[bp] - 1;
-        minT = t;
-      }
-      if (visited[bp] - 1 == t) {
-        cout << t;
-        return 0;
-      }
-    }
-    // cout << visited[bp] - 1 << "\t" << bp << "\t" << t << " " << minT << "\t" << (q ? " MIN" : " ") << "\n";
-  }
-  if (minT == 0) {
-    cout << "-1";
-  } else {
-    cout << minT;
-  }
+  cout << bfs();
 
   return 0;
 }
