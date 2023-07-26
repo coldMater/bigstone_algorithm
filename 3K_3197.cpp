486ec3f04d870bc49cfbc342e937f3de56f44b1b
= <=>
2023-07-27 00:42:44 +0900

    3K_3197 - 3rd try: memory overflow

--- 3K_3197.cpp
+++ 3K_3197.cpp
@@ -5,17 +5,18 @@ using namespace std;
 int r, c;
 const int M = 1501;
 char m[M][M];
-int sVisited[M][M]; // swan visited
+bool visited[M][M];
+bool sVisited[M][M]; // swan visited
 int dy[4] = { -1, 0, 1, 0 };
 int dx[4] = { 0, 1, 0, -1 };
-vector<pair<int, int>> ss; // swans
+pair<int, int> sw; // swan
 queue<pair<int, int>> ims; // ice list to be melted
 
 bool check () {
   fill(&sVisited[0][0], &sVisited[0][0] + M * M, 0);
   queue<pair<int, int>> q;
-  q.push({ ss[0].first, ss[0].second });
-  sVisited[ss[0].first][ss[0].second] = 1;
+  q.push(sw);
+  sVisited[sw.first][sw.second] = 1;
   while (!q.empty()) {
     pair<int, int> prev = q.front();
     q.pop();
@@ -26,7 +27,7 @@ bool check () {
       if (sVisited[ny][nx] != 0) continue;
       if (m[ny][nx] == 'X') continue;
       sVisited[ny][nx] = 1;
-      if (ny == ss[1].first && nx == ss[1].second) { 
+      if (m[ny][nx] == 'L') { 
         return true;
       }
       q.push({ ny, nx });
@@ -39,7 +40,7 @@ bool nearWater(int y, int x) {
   for (int i = 0; i < 4; ++i) {
     int ny = y + dy[i];
     int nx = x + dx[i];
-    if (ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
+    if (ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny][nx] != 0) continue;
     if (m[ny][nx] != 'X') return true;
   }
   return false;
@@ -53,7 +54,7 @@ int main () {
     for (int j = 0; j < c; ++j) {
       m[i][j] = s[j];
       if (m[i][j] == 'L') {
-        ss.push_back({ i, j });
+        sw = { i, j };
       }
     }
   }
@@ -62,6 +63,7 @@ int main () {
     for (int j = 0; j < c; ++j) {
       if (m[i][j] == 'X' && nearWater(i, j)) {
         ims.push({ i, j });
+        visited[i][j] = 1;
       }
     }
   }
@@ -81,7 +83,10 @@ int main () {
       for (int j = 0; j < 4; ++j) {
         int ny = ice.first + dy[j];
         int nx = ice.second + dx[j];
-        if (m[ny][nx] == 'X') ims.push({ ny, nx });
+        if (m[ny][nx] == 'X') { 
+          ims.push({ ny, nx });
+          visited[ny][nx] = 1;
+        };
       }
     }
   }
