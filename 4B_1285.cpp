c5b31708ea9334a9088a35d92f6b3fc1d268d853
= <=>
2023-08-18 00:02:44 +0900

    4B_1285 - 1st try: failed (wrong)

--- 4B_1285.cpp
+++ 4B_1285.cpp
@@ -2,7 +2,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N;
+int r[20];
+int fm = 987654321; //final minimum
+void go (int here) {
+  // cout << here << " " << N << "\n";
+  if (here == N) {
+    int m = 0; // mininum
+    for (int i = 0; i < N; ++i) {
+      int rc = 0;// count of T to row
+      for (int j = 0; j < N; ++j) {
+        if (r[j] & 1 << i) {
+          rc++;
+        }
+      }
+      m += min(N - rc, rc);
+    }
+    fm = min(fm, m);
+    return;
+  }
+  r[here] = ~r[here];
+  go(here + 1);
+  r[here] = ~r[here];
+}
+
 int main () {
-  cout << ~13;
+  cin >> N;
+  for (int i = 0; i < N; ++i) {
+    int c = 0;
+    string s;
+    cin >> s;
+    for (int j = 0; j < N; ++j) {
+      // THTT -> 1101 -> 13
+      if (s[j] == 'T') {
+        c += 1 << j; // c |= 1 << j;
+      }
+    }
+    r[i] = c;
+  }
+
+  go (0);
+  cout << fm;
   return 0;
 }
