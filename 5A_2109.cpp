da4173bac034848bf20d9958c386181b7c59a873
= <=>
2024-01-16 23:50:18 +0900

    5A_2109 - 2nd try: success

--- 5A_2109.cpp
+++ 5A_2109.cpp
@@ -3,27 +3,40 @@
 using namespace std;
 
 int n;
-int p[100'001];
-int md;
+int ret;
 int main () {
   cin >> n;
+  vector<pair<int, int>> dp(n); // day & price
   for (int i = 0; i < n; ++i) {
-    int price;
-    int day;
-    cin >> price >> day;
-    if (p[day] < price) {
-      p[day] = price;
-      md = max(md, day);
+    cin >> dp[i].second >> dp[i].first;
+  }
+
+  sort(dp.begin(), dp.end()); // order by day
+
+  int i = 0;
+  priority_queue<int, vector<int>, greater<int>> prices;
+  for (int d = 1; d <= 10'000; ++d) {
+    while (i < n && dp[i].first == d) {
+      prices.push(dp[i++].second);
+    }
+    while (prices.size() > d) {
+      prices.pop();
     }
   }
 
-  int sum = 0;
-  for (int i = 1; i <= md; ++i) {
-    // cout << "i: " << i << ", sum: " << sum << ", p: " << p[i] << "\n";
-    sum += p[i];
+  while (prices.size()) {
+    // cout << prices.top() << "\n";
+    ret += prices.top();
+    prices.pop();
   }
 
-  cout << sum;
+  // // int sum = 0;
+  // // for (int i = 1; i <= md; ++i) {
+  //   // cout << "i: " << i << ", sum: " << sum << ", p: " << p[i] << "\n";
+  //   // sum += p[i];
+  // // }
+
+  cout << ret;
 
   return 0;
 }
