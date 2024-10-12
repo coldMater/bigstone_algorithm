afc7e0cd5e51e89cc32497ced763da6af8739d4e
= <=>
2024-10-13 03:34:29 +0900

    5T_17143 - nth try - success (boundary condition)

--- 5T_17143.cpp
+++ 5T_17143.cpp
@@ -14,38 +14,15 @@ struct shark {
 
 int R, C, M;
 // vector<shark> ss; // sharks;
-vector<shark> b[101][101];
-vector<shark> nb[101][101]; // next board
-
-void copyAndReset () {
-  for (int i = 1; i <= R; ++i) {
-    for ( int j = 1; j <= C; ++j) {
-      b[i][j] = nb[i][j];
-      nb[i][j].clear();
-    }
-  }
-}
+shark b[101][101];
+shark nb[101][101]; // next board
 
 void drawBoard () {
   for (int i = 1; i <= R; ++i) {
     for (int j = 1; j <= C; ++j ) {
       cout << "[";
-      for (shark s : b[i][j]) {
-        cout << s.idx << (s.d == 4 ? "<" : s.d == 3 ? ">" : s.d == 1 ? "^" : "v");
-      }
-      cout << "]\t";
-    }
-    cout << "\n";
-  }
-  cout << "\n";
-}
-
-void drawNextBoard () {
-  for (int i = 1; i <= R; ++i) {
-    for (int j = 1; j <= C; ++j ) {
-      cout << "[";
-      for (shark s : nb[i][j]) {
-        cout << s.idx << (s.d == 4 ? "<" : s.d == 3 ? ">" : s.d == 1 ? "^" : "v");
+      if (b[i][j].idx > 0) {
+        cout << char(b[i][j].idx + 65 - 1) << (b[i][j].d == 4 ? "<" : b[i][j].d == 3 ? ">" : b[i][j].d == 1 ? "^" : "v");
       }
       cout << "]\t";
     }
@@ -58,31 +35,29 @@ void moveShark (shark* s) {
   // cout << "idx: " << s->idx << ", speed: " << s->s << "\n";
   if (s->d == 3 || s->d == 4) {
     int dedup = s->s % ((C - 1) * 2);
+    // int dedup = s->s;
     while (dedup-- > 0) {
       if (s->d == 3) {
         s->c += 1; // when right
-        // cout << s->c << "->";
-        if (s->c == C) { s->d = 4; continue; } // turn to left
+        if (s->c > C) { s->c = C - 1; s->d = 4; continue; } // turn to left
       }
       if (s->d == 4) {
         s->c -= 1; // when left
-        // cout << s->c << "->";
-        if (s->c == 1) { s->d = 3; continue; } // turn to right
+        if (s->c < 1) { s->c = 2; s->d = 3; continue; } // turn to right
       }
     }
   }
   if (s->d == 1 || s->d == 2) {
     int dedup = s->s % ((R - 1) * 2);
+    // int dedup = s->s;
     while (dedup-- > 0) {
       if (s->d == 2) {
         s->r += 1; // when down
-        // cout << s->r << "->";
-        if (s->r == R) { s->d = 1; continue; } // turn to up
+        if (s->r > R) { s->r = R - 1; s->d = 1; continue; } // turn to up
       }
       if (s->d == 1) {
         s->r -= 1; // when up
-        // cout << s->r << "->";
-        if (s->r == 1) { s->d = 2; continue; } // turn to down
+        if (s->r < 1) { s->r = 2; s->d = 2; continue; } // turn to down
       }
     }
   }
@@ -92,27 +67,17 @@ void moveShark (shark* s) {
 void moveSharks () {
   for (int i = 1; i <= R; ++i) {
     for (int j = 1; j <= C; ++j) {
-      for (shark s : b[i][j]) {
-        moveShark(&s);
-        nb[s.r][s.c].push_back(s);
-      }
-    }
-  }
-}
-
-void battleSharks () {
-  for (int i = 1; i <= R; ++i) {
-    for (int j = 1; j <= C; ++j) {
-      int sc = nb[i][j].size(); // shark count
-      if (sc <= 1) continue;
-      shark largestShark = nb[i][j][0];
-      for (int k = 1; k < sc; ++k) {
-        if (largestShark.z < nb[i][j][k].z) {
-          largestShark = nb[i][j][k];
+      shark s = b[i][j];
+      if (s.idx == 0) continue;
+      moveShark(&s);
+      if (nb[s.r][s.c].idx > 0) {
+        // cout << "Battle! " << nb[s.r][s.c].idx << "(" << nb[s.r][s.c].z << ")" << " VS " << s.idx << "(" << s.z << ")" << "\n";
+        if (nb[s.r][s.c].z < s.z) {
+          nb[s.r][s.c] = s;
         }
+      } else {
+        nb[s.r][s.c] = s;
       }
-      nb[i][j].clear();
-      nb[i][j].push_back(largestShark);
     }
   }
 }
@@ -123,28 +88,28 @@ int main () {
   for (int i = 0; i < M; ++i) {
     shark s;
     cin >> s.r >> s.c >> s.s >> s.d >> s.z;
-    s.idx = i;
-    b[s.r][s.c].push_back(s);
+    s.idx = i + 1; // assigning number to each shark (0: none of shark exist)
+    b[s.r][s.c] = s;
   }
 
+
   int tw = 0; // caught shark's total weight
-  for (int fm = 1; fm <= C; ++fm) { // fisherman's position (equal to day count)
+  for (int fm = 1; fm <= C; ++fm) { // fm: fisherman's position (equal to day count)
+    // cout << "day: " << fm << "\n";
+    // drawBoard();
     for (int i = 1; i <= R; ++i) {
-      if (b[i][fm].size()) {
-        tw += b[i][fm][0].z;
-        b[i][fm].clear();
+      if (b[i][fm].idx > 0) {
+        tw += b[i][fm].z;
+        b[i][fm].idx = 0; // death handling
         break;
       }
     }
 
-
-    // cout << "day: " << fm << "\n";
     // drawBoard();
     moveSharks();
-    // drawNextBoard();
-    battleSharks();
-    // drawNextBoard();
-    copyAndReset();
+    memcpy(b, nb, sizeof(b));
+    memset(nb, 0, sizeof(b));
+    // drawBoard();
   }
 
   cout << tw;
