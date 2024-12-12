1f72534c790e81664f6767632ae5b8b710ec8fe4
= <=>
2024-12-12 23:04:26 +0900

    6G_1072 - 1st try: failed

---
+++ 6G_1072.cpp
@@ -0,0 +1,34 @@
+// https://www.acmicpc.net/problem/1072
+#include <bits/stdc++.h>
+using namespace std;
+
+long long x, y;
+int ans = 999;
+int main () {
+  cin >> x >> y;
+
+  int iz = y * 100  / x; // initial Z
+  int l = 1;
+  int r = 1'000'000'000;
+  while (l <= r) {
+    int mid = (l + r) / 2;
+
+    int z = (y + mid) * 100 / (x + mid);
+
+    if (z == iz) {
+      // no change, need to increase victory count(= mid)
+      l = mid + 1;
+    } else {
+      // changed, need to find minimium victory count
+      ans = min(ans, mid);
+      r = mid - 1;
+    }
+  }
+
+  if (ans == 999) {
+    cout << -1;
+  } else {
+    cout << ans;
+  }
+  return 0;
+}
