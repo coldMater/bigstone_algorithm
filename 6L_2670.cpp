a473834bc318e4c61534f792f0846c34ac3c474f
= <=>
2025-01-05 23:24:00 +0900

    6L_2670 - 1st try: failed

---
+++ 6L_2670.cpp
@@ -0,0 +1,30 @@
+// https://www.acmicpc.net/problem/2670
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+float n[10'000];
+float res;
+int main () {
+  cin >> N;
+  for (int i = 0; i < N; ++i) {
+    cin >> n[i];
+  }
+
+  float s = n[0]; // serial multiplier
+  res = s;
+  for (int i = 1; i < N; ++i) {
+    float next = s * n[i];
+    if (n[i] > next) {
+      s = n[i];
+      // cout << "max: " << res << ", n[i]: " << n[i] << "\n";
+    } else {
+      s = next;
+      // cout << "new: " << s << "\n";
+    }
+    res = max(res, s);
+  }
+  printf("%.3f", res);
+
+  return 0;
+}
---
+++ 6L_2670_1_☠️could not solve myself.md
@@ -0,0 +1,2 @@
+"The idea I couldn't come up with is as follows:
+The replacement idea focuses on identifying when the sequence of consecutive multipliers should end."
