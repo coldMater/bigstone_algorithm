d7af61af138fdfe753ae3a3ec60b9a78b7b788cb
= <=>
2025-02-09 02:43:13 +0900

    7I_4781 - 2nd try: failed

--- 7I_4781.cpp
+++ 7I_4781.cpp
@@ -5,41 +5,29 @@ using namespace std;
 int n;
 double m;
 int dp[10'001];
-int go(vector<pair<int, int>> cs, int rm) { // remained money
-  if (rm < 0) return 0;
-
-  int& dpv = dp[rm];
-  if (dpv != -1) return dpv;
-  dpv = 0;
-  for (int i = 0; i < cs.size(); ++i) {
-    int cal = cs[i].first;
-    double price = cs[i].second;
-    if (price <= rm) {
-      int r = go(cs, rm - price) + cal;
-      dpv = max(dpv, r);
-    }
-  }
-  return dpv;
-}
 
 int main () {
   while (true) {
     cin >> n >> m;
-    fill(dp, dp + 10'001, -1);
+    fill(dp, dp + 10'001, 0);
     if (n == 0 && m == 0) break;
-    vector<pair<int, int>> candies;
+
+    int cost = (int)(m * 100);
     for (int i = 0; i < n; ++i) {
       int cal; // calorie
       double price; // price
       cin >> cal >> price;
-      candies.push_back({ cal, (int)(price * 100) });
+      int p = (int)(price * 100);
+      for (int j = p; j <= cost; ++j) {
+        dp[j] = max(dp[j], dp[j - p] + cal);
+      }
     }
 
-    int totalCals = go(candies, (int)(m * 100));
-    cout << totalCals << "\n";
+    cout << dp[cost] << "\n";
   }
 
   return 0;
 }
 
 // 1st try: timeout (12%)
+// 2nd try: 87% -> rounding error
