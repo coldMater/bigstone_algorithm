105687ab4b3c1d4c529fb04eaf189890517d5ed7
= <=>
2025-02-10 23:48:50 +0900

    7J_12865 - nth try: success

---
+++ 7J_12865.cpp
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/12865
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, K, W, V;
+int dp[100'001];
+int main () {
+  cin >> N >> K;
+  for (int i = 0; i < N; ++i) {
+    cin >> W >> V;
+    for (int j = K; j >= W; --j) { // 🤔 Why iterate backward (K to W) instead of forward (W to K)?  
+      dp[j] = max(dp[j - W] + V, dp[j]);
+      // cout << dp[j] << " ";
+    }
+    // for (int j = 0; j <= K; ++j) {
+    //   // dp[j] = max(dp[j - W] + V, dp[j]);
+    //   cout << dp[j] << " ";
+    // }
+    // cout << "\n";
+  }
+  cout << dp[K];
+  return 0;
+}
