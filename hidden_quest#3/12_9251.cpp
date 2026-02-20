0236727b5f45c8207a138760d347bce229ea7036
= <=>
2026-02-21 00:00:02 +0900

    hidden quest #3 - 9251

--- hidden_quest#3/12_9251.cpp
+++ hidden_quest#3/12_9251.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[1001][1001];
+int arr[1002][1002];
 vector<char> A;
 vector<char> B;
 int main () {
@@ -10,14 +10,16 @@ int main () {
   string t2;
   cin >> t1;
   cin >> t2;
-  A.push_back(' '); B.push_back(' '); // spacer
+  A.push_back(' '); B.push_back(' '); // padding for 1-based indexing
   for (int i = 0; i < t1.length(); ++i) {
     A.push_back(t1.at(i));
+  }
+  for (int i = 0; i < t2.length(); ++i) { // ⚠️ Lengths of A and B can be different
     B.push_back(t2.at(i));
   }
 
   for (int i = 1; i <= t1.length(); ++i) {
-    for (int j = 1; j <= t1.length(); ++j) {
+    for (int j = 1; j <= t2.length(); ++j) {
       if (A[i] == B[j]) {
         arr[i][j] = arr[i - 1][j - 1] + 1;
       } else {
@@ -26,7 +28,7 @@ int main () {
     }
   }
 
-  // { // debugging
+  // { // for debugging
   //   cout << "  ";
   //   for (int i = 0; i <= t1.length(); ++i) {
   //     cout << B[i] << " ";
@@ -35,14 +37,14 @@ int main () {
 
   //   for (int i = 0; i <= t1.length(); ++i) {
   //     cout << A[i] << " ";
-  //     for (int j = 0; j <= t1.length(); ++j) {
+  //     for (int j = 0; j <= t2.length(); ++j) { // fixed: should use t2.length() for columns
   //       cout << arr[i][j] << " ";
   //     }
   //     cout << "\n";
   //   }
   // }
 
-  cout << arr[t1.length()][t1.length()];
+  cout << arr[t1.length()][t2.length()];
 
   return 0;
 }
